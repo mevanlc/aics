e554824cbf737908467f31f7b21b1e568746bf45
Mike Clark <mevanlc@gmail.com>
2026-04-06 09:12:57 -0600

    tighten keymap hint labels for space

--- src/tui/app.rs
+++ src/tui/app.rs
@@ -185,13 +185,13 @@ pub struct App {
 impl App {
     const MAIN_HINTS: [keymap_hint::KeymapHint; 8] = [
         keymap_hint::KeymapHint::new("↑↓", "select"),
-        keymap_hint::KeymapHint::new("Enter", "actions"),
+        keymap_hint::KeymapHint::new("⏎", "actions"),
         keymap_hint::KeymapHint::new("^F", "filters"),
         keymap_hint::KeymapHint::new("^S", "settings"),
-        keymap_hint::KeymapHint::new("^T", "toggle preview"),
+        keymap_hint::KeymapHint::new("^T", "toggle"),
         keymap_hint::KeymapHint::new("^H/^L", "resize"),
         keymap_hint::KeymapHint::new("^C", "quit"),
-        keymap_hint::KeymapHint::new("PgUp/PgDn/Home/End", "scroll preview"),
+        keymap_hint::KeymapHint::new("PgUp/PgDn", "scroll"),
     ];
 
     fn new(
